daace1da77054f7984640f7ab53da87bccff6803
karthik <karthik@karthik>
2020-03-03 11:21:57 +0530

    collections first commit

---
+++ epam_collections/src/main/java/com/collection/infinitelist.java
@@ -0,0 +1,17 @@
+package com.collection;
+
+import java.util.ArrayList;
+
+public class infinitelist {
+public static void main(String args[])
+{
+	ArrayList<String> list=new ArrayList<String>();
+	list.add("epam");
+	list.add("collection");
+	list.add("webinar");
+	for(String x: list)
+	{
+		System.out.print(x+" ");
+	}
+}
+}
